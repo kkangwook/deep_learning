f0664eed0c08723b38883ff0149edcd94a0c96c7
kkangwook <send2woohyun@naver.com>
2025-05-16 12:54:40 +0900

    Update 복습.py

--- study/복습.py
+++ study/복습.py
@@ -2,7 +2,7 @@
 -(x_train,y_train),(x_test,y_test)=keras.datasets.fashion_mnist.load_data() 불러오기
 - 10개 이미지화 -> train세트에서 20% validation 세트 분리
 - 입력층에 들어가는 두가지 방법은?
-- 층 2개, 규제층 하나로 -> 요약정보보기
+- 층 2개, 규제층 하나로 -> 요약정보보기 -> 각 파라미터 개수 나온 원리는?+실제로 계산해보기
 -밑에 애들 넣고 학습시키기
   checkpoint_cb=keras.callbacks.ModelCheckpoint('best-model.h5', save_best_only=True)  
   early_stopping_cb=keras.callbacks.EarlyStopping(patience=2,restore_best_weights=True)
