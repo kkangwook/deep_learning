8da296210dcbadfe16b1133f982aa2620c7f303b
kkangwook <send2woohyun@naver.com>
2025-06-30 12:53:26 +0900

    Update 0_tensorflow기본문법.py

--- study/0_tensorflow기본문법.py
+++ study/0_tensorflow기본문법.py
@@ -28,6 +28,26 @@ print(add_graph_mode(x, y)) #tf.Tensor([ 2.  4.  6.  8. 10.], shape=(5,), dtype=
 # tf결과를 numpy형태로
 result_tensor.numpy() # 결과에 .numpy() 
 
+# numpy를 tf.add와 같은 함수에 넣어도 됨
+
 # 그래프 구조
 노드(점): 값, 연산(+,-), 함수(relu)등 가능
 엣지(선): 텐서데이터 흐름
+
+
+# 그래프 시각화하기
+dir = "C:/ITWILL/logs/calc/" + ***디렉토리_이름 정해주기***
+writer = tf.summary.create_file_writer(dir) #파일 저장 객체와 함수
+tf.summary.trace_on(graph=True, profiler=False)  
+with writer.as_default(): # 블록 안에서 실행되는 연산 그래프를 로그파일에 기록 
+    # Tensor 생성(데이터)  
+    x = tf.constant(1.0, dtype=tf.float32) # 텐서 상수  
+    y = tf.constant(2.0, dtype=tf.float32) # 텐서 상수   
+    # 함수 호출 & Graph 로그파일  
+    result = ***함수***(x, y) # 여기에 함수    
+    tf.summary.trace_export(name="ArithmeticGraph", step=0) 
+print(result) #하면 값나옴
+# anaconda prompt에서 한줄씩
+conda activate tensorflow
+tensorboard --logdir=C:\ITWILL\logs\calc\***아까 정해준 이름***
+-> 뜬 사이트 ctrl+클릭으로 사이트 오픈 -> 시각화된 정보 볼수있음(노도와 엣지로 이루어진)
