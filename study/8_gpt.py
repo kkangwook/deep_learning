d9d5aaa3dca18c852e2eb120775c570f34d16384
kkangwook <send2woohyun@naver.com>
2025-09-27 14:44:18 +0900

    Update 8_gpt.py

--- study/8_gpt.py
+++ study/8_gpt.py
@@ -6,7 +6,7 @@
     =>출력값(batch_size, seq_len, hidden_size=768)
                     ㅣ
       -transformer decoder block X N-
-             -Masked Multi-Head Self-Attention(삼각형의 casual masking적용된)-
+             -Masked Multi-Head Self-Attention(삼각형의 casual masking적용된)-    #여기가 for문 없이 한번에 계산하게끔 해주는곳
              -Residual Connection + LayerNorm(입력 + attention 출력 → 정규화)-
              -Feed-Forward Network (MLP), gelu활성화 사용-
              -Residual Connection + LayerNorm-
@@ -75,7 +75,7 @@ labels    = [love, pizza, very, much, <EOS>]
 outputs = model(input_ids=input_ids, labels=labels)
 loss = outputs.loss  # 모델이 내부적으로 모든 위치에 대해 loss 계산
 => 모델은 자동으로 시퀀스 전체를 한 번에 처리 → 각 위치별 logits 계산 → loss 합산
-=> 따라서 모델 내부에 for문은 필요X (행렬로 한꺼번에 계산하기 때문) !!!!!!!!!!!!!!!!!!!!
+=> 따라서 모델 내부에 for문은 필요X (multihead-self attention+casual masking으로 행렬로 한꺼번에 계산하기 때문) !!!!!!!!!!!!!!!!!!!!
 
 -3. 학습된 모델로 생성시킬때(for문 필요)
 
